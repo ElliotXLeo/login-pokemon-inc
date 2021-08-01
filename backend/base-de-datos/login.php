785cac82e4a4e47da587c0127ed365e5183017f9
ElliotXLeo <elliotgaramendi@gmail.com>
2021-08-01 16:43:20 -0500

    Backend: login con PDO y AJAX

--- backend/base-de-datos/login.php
+++ backend/base-de-datos/login.php
@@ -1,6 +1,28 @@
 <?php
+
+session_start();
+
 require_once "conexion.php";
 $conexion = new Conexion();
 $conectar = $conexion->Conectar();
 
-print_r($conectar);
+$usuario = (isset($_POST["usuario"])) ? $_POST["usuario"] : "";
+$clave = (isset($_POST["clave"])) ? $_POST["clave"] : "";
+
+$claveEncriptada = md5($clave);
+
+$consulta = "SELECT * FROM usuario WHERE usuario='$usuario' AND clave = '$claveEncriptada'";
+
+$resultado = $conectar->prepare($consulta);
+$resultado->execute();
+
+if ($resultado->rowCount() >= 1) {
+  $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+  $_SESSION["usuario"] = $usuario;
+} else {
+  $data = null;
+  $_SESSION["usuario"] = null;
+}
+
+print json_encode($data);
+$conectar = null;
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,8 +5,38 @@ $('#formLogin').submit((e) => {
   if (usuario == "" || clave == "") {
     Swal.fire({
       icon: 'warning',
-      title: 'Debe ingresar su usuario y/o clave.'
+      title: 'Debe ingresar un usuario y/o clave.'
     });
     return false;
+  } else {
+    $.ajax({
+      url: '../backend/base-de-datos/login.php',
+      type: 'POST',
+      datatype: 'json',
+      data: {
+        usuario: usuario,
+        clave: clave
+      },
+      success: (data) => {
+        if (data != 'null') {
+          Swal.fire({
+            icon: 'success',
+            title: '¡Conexión exitosa!.',
+            // confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ingresar'
+          }).then((result) => {
+            if (result.value) {
+              window.location.href = './views/inicio.php';
+            }
+          });
+          ;
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Usuario y/o clave incorrecta.'
+          });
+        }
+      }
+    });
   }
 });
